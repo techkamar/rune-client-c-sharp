82d1d074222334d7fd85dc631f1d0c97949dfcde
K Syed Khamarudheen <techkamar@gmail.com>
2023-11-27 22:30:03 +0530

    FileBrowse added

--- TestAPP/lib/JSONHolder.cs
+++ TestAPP/lib/JSONHolder.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace TestAPP.lib
 {
@@ -18,11 +19,64 @@ namespace TestAPP.lib
             hashtable.Add(key,value);
         }
 
-        public void addIntegerEntry(String key, Int16 value)
+        public void addListEntry(String key, ArrayList value)
+        {
+            hashtable.Add(key, value);
+        }
+        public void addIntegerEntry(String key, Int32 value)
         {
             hashtable.Add(key, value);
         }
 
+        public String getParsedHashTable(Hashtable map)
+        {
+            if (map.Count == 0)
+            {
+                return "{}";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{");
+
+            foreach (DictionaryEntry element in map)
+            {
+                if (element.Value.GetType().ToString() == "System.String")
+                {
+                    sb.Append(String.Format(" \"{0}\":\"{1}\",", element.Key, element.Value));
+                }
+                else
+                {
+                    sb.Append(String.Format(" \"{0}\":{1},", element.Key, element.Value));
+                }
+
+            }
+            return sb.ToString().Substring(0, sb.Length - 1)+"}";
+        }
+        public String getParsedList(ArrayList value)
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            if(value.Count == 0)
+            {
+                return "";
+            }
+
+            for(int index=0; index<value.Count; index++)
+            {
+                if (value[index].GetType().ToString() == "System.String")
+                {
+                    sb.Append(String.Format("\"{0}\",", value[index].ToString()));
+                }
+                else
+                if (value[index].GetType().ToString() == "System.Collections.Hashtable")
+                {
+                    sb.Append(getParsedHashTable((Hashtable)value[index]) +",");
+                }
+            }
+
+            return sb.ToString().Substring(0, sb.Length - 1);
+        }
         public String getParsedJSONString()
         {
             String output = "{";
@@ -32,6 +86,11 @@ namespace TestAPP.lib
                 if (element.Value.GetType().ToString() == "System.String")
                 {
                     output += String.Format(" \"{0}\":\"{1}\",", element.Key, element.Value);
+                }
+                else
+                if(element.Value.GetType().ToString() == "System.Collections.ArrayList")
+                {
+                    output += String.Format(" \"{0}\":[{1}],", element.Key, getParsedList((ArrayList)element.Value));
                 }
                 else
                 {
